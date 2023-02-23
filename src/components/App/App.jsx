8fceda4391d87ff0b23f5cf164e7bc3969225c49
Natalia Levenets <levenets.lot@gmail.com>
2023-02-23 03:24:44 +0200

    lazy suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,49 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import Header from '../Header/Header';
-import Cast from '../Cast/Cast';
-import Reviews from '../Reviews/Reviews';
-import  Home from '../../pages/Home';
-import  Movies from '../../pages/Movies';
-import  MovieDetails from '../../pages/MovieDetails';
+const LazyHeader = lazy(() => import('../Header/Header'));
+const LazyHome = lazy(() => import('../../pages/Home'));
+const LazyMovies = lazy(() => import('../../pages/Movies'));
+const LazyMovieDetails = lazy(() => import('../../pages/MovieDetails'));
+const LazyCast = lazy(() => import('../Cast/Cast'));
+const LazyReviews = lazy(() => import('../Reviews/Reviews'));
 
 export const App = () => {
   return (
+    <Suspense>
     <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />}/>
-        <Route path="/movies/:movieId/reviews" element={<Reviews/>} />
+      <Route path="/" element={<LazyHeader />}>
+        <Route index element={<LazyHome />} />
+        <Route path="/movies" element={<LazyMovies />} />
+        <Route path="/movies/:movieId" element={<LazyMovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<LazyCast />}/>
+        <Route path="/movies/:movieId/reviews" element={<LazyReviews/>} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace={true} />} />
     </Routes>
+    </Suspense>
   );
 };
-// 
-// const LazyHeader = lazy(() => import('../Header/Header'));
-// const LazyHome = lazy(() => import('pages'));
-// const LazyMovies = lazy(() => import('pages'));
-// const LazyMovieDetails = lazy(() => import('pages'));
-// const LazyCast = lazy(() => import('../Cast/Cast'));
-// const LazyReviews = lazy(() => import('../Reviews/Reviews'));
+
+// import Header from '../Header/Header';
+// import Cast from '../Cast/Cast';
+// import Reviews from '../Reviews/Reviews';
+// import  Home from '../../pages/Home';
+// import  Movies from '../../pages/Movies';
+// import  MovieDetails from '../../pages/MovieDetails';
 
 // export const App = () => {
 //   return (
-//     <Suspense>
 //     <Routes>
-//       <Route path="/" element={<LazyHeader />}>
-//         <Route index element={<LazyHome />} />
-//         <Route path="/movies" element={<LazyMovies />} />
-//         <Route path="/movies/:movieId" element={<LazyMovieDetails />} />
-//         <Route path="/movies/:movieId/cast" element={<LazyCast />}/>
-//         <Route path="/movies/:movieId/reviews" element={<LazyReviews/>} />
+//       <Route path="/" element={<Header />}>
+//         <Route index element={<Home />} />
+//         <Route path="/movies" element={<Movies />} />
+//         <Route path="/movies/:movieId" element={<MovieDetails />} />
+//         <Route path="/movies/:movieId/cast" element={<Cast />}/>
+//         <Route path="/movies/:movieId/reviews" element={<Reviews/>} />
 //       </Route>
 //       <Route path="*" element={<Navigate to="/" replace={true} />} />
 //     </Routes>
-//     </Suspense>
 //   );
 // };
-
-
+// 
